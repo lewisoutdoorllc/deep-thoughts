33eaf239c876323b043a25bc855676da18f7ed9d
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-10 18:54:19 -0400

    changes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,30 @@
 const { User, Thought } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+  Query: {
+    // logic here remains the same
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return {token, user};
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrest Credentials");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+      const token = signToken(user);
+      return {token, user};
+    },
+  },
   Query: {
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
